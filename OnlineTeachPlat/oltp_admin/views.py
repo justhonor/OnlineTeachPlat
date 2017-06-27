cd083746adfd68696878680d88e7d1a61e19405a
aiapple <aiapple@aiapple.(none)>
2017-06-27 22:59:56 +0800

    完成新增和删除功能

--- OnlineTeachPlat/oltp_admin/views.py
+++ OnlineTeachPlat/oltp_admin/views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.http import HttpResponseRedirect,HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required
 
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
@@ -10,11 +10,12 @@ from oltp_admin.form import RegisterForm
 
 from oltp_admin.models import UserInfo
 from django.contrib.auth.models import User, Group
-import pdb; 
+import pdb
 
 from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 # superuser root:root_11111 admin:admin_11111
+
 
 def index(request):
     '''主视图'''
@@ -29,22 +30,22 @@ def index(request):
     for user in users:
         pass
         # pdb.set_trace()
-        #groups[user.username]=user.groups.all()[0].name
-    
+        # groups[user.username]=user.groups.all()[0].name
+
     if request.user.is_authenticated():
-    	if request.user.username == 'admin':
-    	    # 管理用户
-    		return render(request,"oltp_admin/admin_index.html",{'users':users}) 
-    	else:
-    		# 普通用户
-    		return render(request,"oltp_admin/index.html")	
+        if request.user.username == 'admin':
+            # 管理用户
+            return render(request, "oltp_admin/admin_index.html", {'users': users})
+        else:
+            # 普通用户
+            return render(request, "oltp_admin/index.html")
     else:
-    	return HttpResponseRedirect("/oltp/login")
+        return HttpResponseRedirect("/oltp/login")
 
 
 def logout(request):
-	auth_logout(request)
-	return render(request, "oltp_admin/logout.html")
+    auth_logout(request)
+    return render(request, "oltp_admin/logout.html")
 
 
 def register(request):
@@ -57,33 +58,33 @@ def register(request):
         form = RegisterForm(request.POST.copy())
         # import pdb; pdb.set_trace()
         if form.is_valid():
-        	# 验证通过，创建用户
+            # 验证通过，创建用户
             form.save()
             # 登录
             if _login(request, form.cleaned_data['username'], form.cleaned_data['password'], template_var):
-                return render(request,"oltp_admin/index.html")
+                return render(request, "oltp_admin/index.html")
 
     template_var["form"] = form
-
     return render(request, "oltp_admin/register.html", template_var)
 
 
 def login(request):
     '''登陆视图'''
-    template_var={}
+    template_var = {}
     if request.method == 'POST':
         username = request.POST.get("username")
         password = request.POST.get("password")
-        if _login(request,username,password,template_var):
+        if _login(request, username, password, template_var):
             try:
                 tmp = request.GET['next']
                 return HttpResponseRedirect(tmp)
             except:
                 return HttpResponseRedirect("/oltp/")
- 
-        template_var.update({"username":username}) 
 
-    return render(request,"oltp_admin/login.html",template_var)
+        template_var.update({"username": username})
+
+    return render(request, "oltp_admin/login.html", template_var)
+
 
 def _login(request, username, password, dict_var):
     '''登陆核心方法'''
@@ -100,11 +101,56 @@ def _login(request, username, password, dict_var):
         dict_var["error"] = u'用户' + username + u'不存在或密码错误'
     return ret
 
+
 @csrf_exempt
 def admin(request):
     '''管理用户'''
-    print "管理用户"
-    import pdb; pdb.set_trace()
-    return HttpResponse(content="recieve")
-    
 
+    # type 值控制具体操作
+    # 删除选中用户
+    if request.method == 'POST' and request.POST.get("type") == "delete":
+        usernames = request.POST.get("usernames")
+        # import pdb; pdb.set_trace()
+        Usernames = usernames.split(',')[:-1]
+        for user in Usernames:
+            print "delete ....%s" % (user)
+            if del_user(request, user):
+                print "delete %s sucess" % (user)
+            else:
+                return HttpResponse(content="delete %s failed" % (user))
+        return HttpResponse(content="delete %s sucess" % (user))
+    # 新增用户
+    elif request.method == 'POST' and request.POST.get("type") == "add":
+        if add_user(request):
+            print "add %s sucess" % (request.POST.get("username"))
+            return HttpResponse(content="Add %s sucess" % (request.POST.get("username")))
+        else:
+            return HttpResponse(content="add %s failed" % (request.POST.get("username")))
+
+
+def del_user(request, username):
+    try:
+        # import pdb; pdb.set_trace()
+        u = User.objects.get(username=username)
+        u.delete()
+        return True
+    except Exception as e:
+        return False
+
+
+def add_user(request):
+    try:
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        user = User.objects.create_user(username, email, password)
+
+        # Add group_name for user
+        if request.POST.get("group") == "学生":
+            user.group_name = "学生"
+        elif request.POST.get("group") == "老师":
+            user.group_name = "老师"
+        user.save()
+        return True
+    except Exception as e:
+        return False
--- OnlineTeachPlat/static/libs/js/oltp.js
+++ OnlineTeachPlat/static/libs/js/oltp.js
@@ -1,31 +1,116 @@
 
 $(document).ready(function(){
-  $("tr.list").click(function(e){
-  	$(this).children('td').toggleClass("selected")
+	/************************************
+	*     渲染选中行 
+	*
+	**************************************/
+	$("tr.list").click(function(e){
+  		$(this).children('td').toggleClass("selected")
+  	});
+
+/************************************
+*     新增用户
+*     admin_index.html --> views.admin
+**************************************/
+$(function (){ // 注意dialog需要外层function包裹
+  //注册对话框
+  $("#dialog").dialog({
+   //设置对话框打开的方式 不是自动打开
+   autoOpen:false,
+   show:"blind",
+   hide:"explode",
+   modal:true,
+   buttons:[
+    {
+     text:"添加",
+     click:function (){
+      
+      //获取姓名
+      var dia_username=$("input[id=dia_username]").val();
+      //获取邮箱
+      var dia_email=$("input[id=dia_email]").val();
+      //获取密码
+      var dia_password=$("input[id=dia_password]").val();
+      //获取类型
+      var dia_student=$("input[id=dia_student]").val();
+      var dia_teacher=$("input[id_student]").val();
+      //状态
+      var status = "True"
+	  var group = ""
+      if (dia_student)
+      {group = "学生"}
+  	  else if(dia_teacher)
+  	  {group = "学生"}
+  	  else{group = "null"}
+      
+      data={
+      	type:"add",
+      	username:dia_username,
+      	email:dia_email,
+      	password:dia_password,
+      	group:group
+      }
+
+	    $.post("/oltp/admin/",data,function(data1,status){
+	    		// $(this).dialog("close");
+	   	  		alert("Data: " + data1 + "\nStatus: " + status);
+		})
+
+		$(this).dialog("close");
+    }
+
+    },{
+       text:"取消",
+       click:function(){
+        $(this).dialog("close");
+        }
+     }
+    ],
+   closeOnEscape:false, //是否esc
+   title:"添加用户操作界面", //对话框标题
+   position:"center",//对话框弹出的位置 top left right center bottom 默认是center
+   width:500, //对话框宽度
+   height:330, //对话框高度
+   resizable:false, //是否可以改变大小操作  默认true
+   }); 
+  
+  //触发连接的事件 当点击连接打开一个对话
+  $("#dialog_link").click(function (){
+   //open参数 作用：打开对话框
+   $("#dialog").dialog("open");
   });
+ });
+
+/************************************
+*     删除选中的用户
+*     admin_index.html --> views.admin
+**************************************/
 
   $("#id_delete").click(function(e){
-  	console.log(e);
+  	// console.log(e);
   	$('td').nextAll('.selected').parent().hide()
 
   	var num=$('.selected').length
-  	var users=new Array();
   	var j=0;
 
-  	var data = [];
+  	var data = {type:"delete"};
+  	var str  = ""
+  	var sp = ","
   	// 取数据
   	for (var i=0;i<num;i=i+4)
   	{
-  		users[j]=$(".selected:eq("+i+")").text()
+  		a = $(".selected:eq("+i+")").text()
   		j = j + 1;
   		num = num - 1;
+  		str = str.concat(a).concat(sp)
+  		// data[j] = $(".selected:eq("+i+")").text()
   	}
+  	data["usernames"] = str
 
-  	json=JSON.stringify(users)
-  	console.log(users)
-  	console.log(json)
+  	// console.log(str)
+  	// console.log(data)
 	// 后台删
-	$.post("/oltp/admin/",json,function(data1,status){
+	$.post("/oltp/admin/",data,function(data1,status){
 		alert("Data: " + data1 + "\nStatus: " + status);
 	})
 
